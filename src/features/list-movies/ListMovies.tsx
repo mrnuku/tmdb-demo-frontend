ea654affd2577b0e6f9746413954dd51c5d866e3
mrnuku <mrnuku@gmail.com>
2022-12-05 00:38:53 +0100

    add better design to movie list

--- src/features/list-movies/ListMovies.tsx
+++ src/features/list-movies/ListMovies.tsx
@@ -3,10 +3,22 @@ import {
   SearchResultRecord,
   selectSearchResult,
 } from './listMoviesSlice';
-// import styles from './ListMovies.module.css';
+import styles from './ListMovies.module.css';
 
 function ListMovieRecord(props: any) {
-  return <li>{ props.record.title }</li>;
+  return (
+    <section className={styles.section}>
+      <img src={props.record.poster_path ? "https://image.tmdb.org/t/p/w500" + props.record.poster_path : "https://via.placeholder.com/200C/O"} className={styles.image} />
+      <div className={styles.content}>
+        <h2 className={styles.title}>
+          { props.record.title }
+        </h2>
+        <p className={styles.paragraph}>
+          { props.record.overview }
+        </p>
+      </div>
+    </section>
+);
 }
 
 export function ListMovies() {
@@ -14,9 +26,9 @@ export function ListMovies() {
 
   return (
     <>
-      <ul>
+      <div className={styles.container}>
         {searchResult !== null ? searchResult.results.map((record: SearchResultRecord) => <ListMovieRecord key={record.id} record={record} />) : null}
-      </ul>
+      </div>
     </>
   );
 }
